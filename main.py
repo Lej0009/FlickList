6fff31bda9993bd9e1b01d6e0d5bb1312a29b2f5
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-03 15:59:09 -0400

    Add dropdown for removing movie from WatchList

--- main.py
+++ main.py
@@ -38,16 +38,52 @@ add_form = """
 
 # a form for crossing off watched movies
 crossoff_form = """
+    <form action="/crossoff" method="post">
+        <label for="crossed-off-movie">
+        I want to cross off
+        <select name="movies" size="4" multiple>
+            <option value="Pineapple Express">Pineapple Express</option>
+            <option value="Hot Tub Time Machine">Hot Tub Time Machine</option>
+            <option value="Step Brothers">Step Brothers</option>
+            <option value="The Other Guys">The Other Guys</option>
+        </select>
+        </label>
+        <input type="submit" value="Remove It">
+    </form>"""
 
-"""
+# i=0
+# str = ""
+# movies = ['Pineapple Express', 'Step Brothers', 'Hot Tub Time Machine', 'The Other Guys']
+
+# for movie in movies:
+#     str += "<option value="
+#     str += movie
+#     str += ">"
+#     str += movies[i]
+#     str += "</option>"
+#     i += 1
+#     """
+    # </select>
+    # </label>
+    # <input type="submit" value="Remove It">
+    # </form>
+
 
 # TODO:
 # Finish filling in the function below so that the user will see a message like:
 # "Star Wars has been crossed off your watchlist".
 # And create a route above the function definition to receive and handle the request from 
 # your crossoff_form.
+@app.route("/crossoff", methods=['POST'])
 def crossoff_movie():
-    crossed_off_movie = request.form['crossed-off-movie']    
+    crossed_off_movie = request.form['crossed-off-movie'] 
+
+    # build response content
+    crossed_off_movie_element = "<strong>" + cross_off_movie + "</strong>"
+    sentence = "<strike>" + crossed_off_movie_element + "</strike>" + " has been crossed off your Watchlist!"
+    content = page_header + "<p>" + sentence + "</p>" + page_footer
+
+    return content   
 
 # TODO:
 # modify the crossoff_form above to use a dropdown (<select>) instead of
@@ -70,9 +106,8 @@ def index():
     edit_header = "<h2>Edit My Watchlist</h2>"
 
     # build the response string
-    content = page_header + edit_header + add_form + page_footer
+    content = page_header + edit_header + add_form + crossoff_form + page_footer
 
     return content
-
 
 app.run()
